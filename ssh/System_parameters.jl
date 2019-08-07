4744eca48a6db285c687306fdb59c80f80271889
Akira-exo <47740941+Akira-exo@users.noreply.github.com>
2019-08-07 15:22:16 +0200

    Update System_parameters.jl

--- ssh/System_parameters.jl
+++ ssh/System_parameters.jl
@@ -11,10 +11,24 @@ Function to call worker 'i' to perform the Task with # or ID 'i'
 	#-----------------------
 
         #NOTE: WORKER'S ID = myid()
-	#SYSTEM PARAMETERS: 
+
         
-        println("starting my job $(jobID) at time $(now()) ")
-        
+        println("starting my job $(jobID) at $(gethostname()) on time $(now()) ")
+
+        #SYSTEM PARAMETERS: 
+        J_x,J_y,M,ϵ,p,scale,q = get_SystemParameters()
+
+	𝐌= assign_M(M,J_y,Ly,p)
+	𝐉,𝐕,𝚵,𝐖t=assign_J(J_x,Ly)
+
+	calc_LyapunovList(𝐌,𝐕,𝚵,𝐖t,ϵ,Ly_list,scale,W,dir_name,q,jobID)
+        println("finishing my job $(jobID) at $(gethostname()) on time $(now()) ")
+
+
+end
+
+@everywhere function get_SystemParamaters()
+	
 	σ_x= [0 1;1 0]
 	σ_y= [0 -1im; 1im 0 ]	
 	σ_z =[1 0; 0 -1]
@@ -24,23 +38,11 @@ Function to call worker 'i' to perform the Task with # or ID 'i'
 	J_y = 1im/2 *( σ_y + 1im* σ_z)
 	M = (2 - m) *σ_z
 	
-        
-        ϵ,p,scale,q = get_SystemParameters()
-	Nx = scale*Ly_list;
-
-	𝐌= assign_M(M,J_y,Ly,p)
-	𝐉,𝐕,𝚵,𝐖t=assign_J(J_x,Ly)
-
-	calc_LyapunovList(𝐌,𝐕,𝚵,𝐖t,ϵ,Ly,Nx,W,dir_name,q,jobID)
-        println("finishing my job $(jobID) at time $(now()) ")
-
-
-end
-
-@everywhere function get_SystemParamaters()
-	
 	ϵ = 0.0    #always probing at the middle of the gap
 	p= 0  # PBC OFF:0, PBC ON:1
 	scale=100    #Nx = scale*Ly;
 	q=3;
+	
+	return(J_x,J_y,M,ϵ,p,scale,q)
+	
 end

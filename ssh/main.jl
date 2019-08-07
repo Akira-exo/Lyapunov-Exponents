2e1d5199cec9e13af4ebdcb97f95cd87e8857dbd
Akira-exo <47740941+Akira-exo@users.noreply.github.com>
2019-08-07 18:01:39 +0200

    Update main.jl

--- ssh/main.jl
+++ ssh/main.jl
@@ -118,12 +118,12 @@ Function to call worker 'i' to perform the job with jobID 'i' each job = one (m,
         println("starting my job $(jobID) at $(gethostname()) on time $(now()) ")
 
         #SYSTEM PARAMETERS: 
-        J_x,J_y,M,ϵ,p,scale,q = get_SystemParameters()
+        J_x,J_y,M,ϵ,p,scale,q = get_SystemParameters(m)
         for Ly in Ly_list
 		𝐌= assign_M(M,J_y,Ly,p)
 		𝐉,𝐕,𝚵,𝐖t=assign_J(J_x,Ly)
         	Nx=scale*Ly
-		λ_list,Q_prev=calc_LyapunovList(𝐌,𝐕,𝚵,𝐖t,ϵ,Ly,Nx,W,q)
+		λ_list,Q_prev=get_LyapunovList(𝐌,𝐕,𝚵,𝐖t,ϵ,Ly,Nx,W,q)
 	        filename = string(dir_name,"/λ_list/λ(m,W,Ly)=(",jobID,", ",Ly,")" )
                 writedlm(filename,λ_list, ", ")
                 filename = string(dir_name,"/Q_prev/Q(m,W,Ly)=(",jobID,", ",Ly,")" )

7c944c6d806ff0fb8caaaf4f2f0dae6318bf78b8
Akira-exo <47740941+Akira-exo@users.noreply.github.com>
2019-08-08 13:55:01 +0200

    Update System_parameters.jl

--- CHEOPS/System_parameters.jl
+++ CHEOPS/System_parameters.jl
@@ -2,17 +2,12 @@
 
 #=
 
-Function to call worker 'i' to perform the Task with # or ID 'i'
+Function to set System Parameters
 
 =#
 
-@everywhere function perform_Task(m::Float64,W::Float64,Ly::Int64,jobID::Int64,dir_name::String)
-
-	#-----------------------
-	#SYSTEM PARAMETERS: 
-        
-
-
+@everywhere function get_SystemParameters(m)
+	
 	σ_x= [0 1;1 0]
 	σ_y= [0 -1im; 1im 0 ]	
 	σ_z =[1 0; 0 -1]
@@ -21,22 +16,12 @@ Function to call worker 'i' to perform the Task with # or ID 'i'
 	J_x = -(1im/2 )*( σ_x - 1im*σ_z)
 	J_y = 1im/2 *( σ_y + 1im* σ_z)
 	M = (2 - m) *σ_z
-
-	ϵ = 0.0 #always probing at the middle of the gap
 	
-        p= 0 # PBC OFF:0, PBC ON:1
-
-	Nx = 100*Ly;
-	q=3;	
-        
-
-
-	𝐌= assign_M(M,J_y,Ly,p)
-	𝐉,𝐕,𝚵,𝐖t=assign_J(J_x,Ly)
-
-	calc_LyapunovList(𝐌,𝐕,𝚵,𝐖t,ϵ,Ly,Nx,W,dir_name,q,jobID)
-
-
-
+	ϵ = 0.0    #always probing at the middle of the gap
+	p= 0  # PBC OFF:0, PBC ON:1
+	scale=100    #Nx = scale*Ly;
+	q=3;
+	
+	return(J_x,J_y,M,ϵ,p,scale,q)
+	
 end
-
